ffae43a534bb918b7a09d87f293918237774288d
William Blanchette <wb@wblanchette.com>
2016-07-15 15:51:46 -0600

    Fixed typo

---
+++ app.js
@@ -0,0 +1,47 @@
+const config = {
+  rateLimit: 60 * 60 * 1000 // Only run once per hour
+};
+
+const jsdom = require('jsdom');
+const express = require('express');
+const vm = require('vm');
+
+const app = express();
+const cache = {};
+
+// Fetch my beer count from untappd
+
+cache.beers = {
+  url: 'https://untappd.com/user/wblanchette',
+  expression: `window.document.querySelector('.stats [data-href=":stats/beerhistory"]').innerHTML.replace(/[^0-9]/g, '');`
+};
+
+app.get('/:token', (req, res) => {
+  const job = cache[req.params.token];
+
+  if (job) {
+    const now = (new Date()).getTime();
+    if (!job.data || (job.timstamp && now - job.timestamp >= config.rateLimit)) {
+      jsdom.env({
+        url: job.url,
+        features: {},
+        done: (err, window) => {
+          try {
+            const script = new vm.Script(job.expression, {});
+            job.data = jsdom.evalVMScript(window, script);
+            job.timestamp = now;
+            res.send(job.data);
+          } catch(e) {
+            res.status(500).end();
+          }
+        }
+      });
+    } else {
+      res.send(job.data);
+    }
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {});
